344a359460b9ae4df1a3bb4257f51f362d0914cd
yogireddy <yogeswara.kandati@cashe.co.in>
2018-08-11 08:27:43 +0530

    files added

---
+++ src/test/java/prj/it/WebappIT.java
@@ -0,0 +1,30 @@
+package prj.it;
+
+import java.net.URL;
+import java.net.HttpURLConnection;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class WebappIT
+{
+    private String baseUrl;
+
+    @Before
+    public void initializeTest() throws Exception
+    {
+        String host = System.getProperty("hostname");
+        String port = System.getProperty("servlet.port");
+        this.baseUrl = "http://" + host + ":" + port + "/dockprj";
+    }
+
+    @Test
+    public void callIndexPage() throws Exception
+    {
+        URL url = new URL(this.baseUrl);
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.connect();
+        Assert.assertEquals(200, connection.getResponseCode());
+    }
+}
